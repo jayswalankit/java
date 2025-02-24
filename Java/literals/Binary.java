31eecd33924c4d6f02ae65f2222516341d414431
ankit <jayswalankit94@gmail.com>
2025-02-24 21:54:02 +0530

    change

--- Java/literals/Binary.java
+++ Java/literals/Binary.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class Binary {
    public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
-    int b=0b10100;
-    System.out.println("value of decimal"+b);
+
+    System.out.println("enter your binary no");
+    int a=sc.nextInt();
+    System.out.println(a);
 
    } 
 }
