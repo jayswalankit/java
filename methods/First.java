7244aade777267f77704a8214ed8c08f0355edaa
ankit <jayswalankit94@gmail.com>
2025-03-01 11:25:35 +0530

    update

--- methods/First.java
+++
@@ -1,9 +0,0 @@
-
-public class First {
-    public static void main(String[] args) {
-        String str="Welcome to world";
-        System.out.println("length is : "+str.length());
-        System.out.println("square root of 25 is : "+Math.sqrt(25));
-        System.out.println(str);
-    }
-}
